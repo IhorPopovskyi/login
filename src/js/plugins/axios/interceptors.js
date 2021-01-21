fb5d3202255ca424de9f5361d4be1aa6b09b0ac5
Ihor Popovskyi <57353154+IhorPopovskyi@users.noreply.github.com>
2021-01-21 13:44:32 +0200

    created temolates

---
+++ src/js/plugins/axios/interceptors.js
@@ -0,0 +1,38 @@
+const lsTolkenKey = "my_app_token";
+
+function setToken(req) {
+    const isAuthUrl = res.url.includes("auth");
+
+    if (!isAuthUrl) {
+        const token = localStorage.getItem(lsTolkenKey);
+        req.headers["x-access-token"] = token;
+    }
+
+    return req;
+}
+
+function setTokenOnLogin(res) {
+    const isLoginUrl = res.config.url.includes("login");
+
+    if (isLoginUrl) {
+        const token = res.data.token;
+        localStorage.setItem(lsTolkenKey, token);
+    }
+
+    return res;
+}
+
+function getClearResponse(res) {
+    return res.data;
+}
+
+function onError(err) {
+    console.dir(err);
+    return Promise.reject(err);
+}
+
+export default function (axios) {
+    axios.interceptors.request.use(setToken);
+    axios.interceptors.response.use(setTokenOnLogin);
+    axios.interceptors.response.use(getClearResponse, onError);
+}
